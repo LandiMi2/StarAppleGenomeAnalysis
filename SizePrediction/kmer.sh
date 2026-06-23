bdb73ffb51f505660e6eaa2104e1f81576d7a6d1
Michael Landi <47971974+LandiMi2@users.noreply.github.com>
2026-06-23 16:42:44 +0300

    Update kmer.sh

--- SizePrediction/kmer.sh
+++ SizePrediction/kmer.sh
@@ -5,8 +5,8 @@ jellyfish histo -t 20 StarApple.jr > StarApple.histo
 /usr/bin/Rscript /data01/mlandi/software/genomescope2.0/genomescope.R -i StarApple.histo -o StarApple -k 21
 
 #Ploidy estimation using smudgeplot v0.5.4 - figure 2b
-FastK -v -t4 -k31 -M16 -T4 /data01/mlandi/Inqaba/data/HiFi_filtered/filtered_cells_merged.fastq.gz -NStatKmer21
-smudgeplot.py hetmers -L 12 -t 4 -o kmerpairs --verbose StatKmer31
-python3.11 /usr/local/bin/smudgeplot.py all -o trialKmer31 kmerpairs.smu -ylim 100
+FastK -v -t4 -k21 -M16 -T4 /data01/mlandi/Inqaba/data/HiFi_filtered/filtered_cells_merged.fastq.gz -NStatKmer21
+smudgeplot.py hetmers -L 12 -t 4 -o kmerpairs --verbose StatKmer21
+python3.11 /usr/local/bin/smudgeplot.py all -o trialKmer21 kmerpairs.smu -ylim 100
 
 
